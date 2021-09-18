6aad73d5af44ab8005c5ac04dd87a496353b880a
Supriya <supriya17feb99@gmail.com>
2021-09-19 01:32:51 +0530

    modified registration validations

--- UserRegistration/registrationValidations.js
+++ UserRegistration/registrationValidations.js
@@ -1,19 +1,4 @@
 
- $(document).ready(function(){
-    
-
-;
-    $("#registerBtn").click(function () {
-   
-        // validateName();
-        // validateGender();
-        // validateEmail();
-        // validatePhoneNum();
-    });
-
-
-  });
-
  function validateName(){
   
         var regexName = /^[a-zA-Z\s]+$/;
@@ -26,48 +11,6 @@
         else if(!regexName.test(uname))
            alert("Name can only have alphabets!");
     
-    }
-
-
-
-
-     function validateGender(){
-        var gender = $('#genderId').val();
-        if(gender == "" || gender == null)
-          { 
-             alert("Gender can't be empty!");
-            return false;
-        }
-        else
-         return true;
-        }
-    
-    function validateEmail(){
-     var emailRegex  = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-     if(!emailRegex.test($('#emailId').val()))
-       {
-        alert("Invalid email!");
-        return false;
-       } 
-       else
-         return true;
-
-    }
-
-    function validatePhoneNum(){
-         phoneRegex = /^\d+$/;
-        if(!$('#phoneNumberId').val().length == 10){
-            alert("Phone Number must be 10 digits!");
-            return false;
-        }
-           
-        else if(!phoneRegex.test($('#phoneNumberId').val())){
-            alert("Phone num must only have digits!"); 
-            return false;
-        }
-        else
-        return true;
-           
     }
 
     function checkPass(){
@@ -99,14 +42,10 @@
 
     function validateform(){
 
-       if( checkPass() &&  validateAddress())
+       if( checkPass() && validateAddress())
            return true;
 
-        // if(validateName() && validateGender() && validateEmail() && validatePhoneNum())
-        //    return true;
-        // else
-        //    return false;
-
-      
+       else
+          return false;
 
     }
