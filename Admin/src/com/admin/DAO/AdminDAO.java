32ec3c18a10b58bb26b5c33a1f3785378855c9d5
Pranav Kulkarni <phkulkarni05@gmail.com>
2021-09-25 14:51:02 +0530

    Final integrated project upload without contact

--- Admin/src/com/admin/DAO/AdminDAO.java
+++
@@ -1,9 +0,0 @@
-package com.admin.DAO;
-
-import com.admin.model.Admin;
-
-public interface AdminDAO {
-	void createConnection();
-	Admin getAdminByCredentials(String username , String password);
-	void closeConnection();
-}
--- Admin/src/com/admin/factory/AdminFactory.java
+++
@@ -1,9 +0,0 @@
-package com.admin.factory;
-
-import com.admin.DAO.impl.AdminDAOImpl;
-
-public class AdminFactory {
-	public static  AdminDAOImpl getAdminImpl() {
-		return new AdminDAOImpl();
-	}
-}
