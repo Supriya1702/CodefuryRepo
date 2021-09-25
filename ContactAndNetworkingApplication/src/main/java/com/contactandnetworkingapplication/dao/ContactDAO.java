6a6d405c398df5317dd639415558ec8c3f8748e7
simran1311 <32571418+simran1311@users.noreply.github.com>
2021-09-26 01:07:29 +0530

    Code Cleanup

--- ContactAndNetworkingApplication/src/main/java/com/contactandnetworkingapplication/dao/ContactDAO.java
+++ ContactAndNetworkingApplication/src/main/java/com/contactandnetworkingapplication/dao/ContactDAO.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 import com.contactandnetworkingapplication.model.Contacts;
 
+//creating interface for Contact Functionalities.
 public interface ContactDAO {
 	int addContact(Contacts contact) throws Exception ;
-	void updateContactDetails();
 	List<Contacts> getContacts(int id) throws Exception;
 	void deleteContact(List<Integer> contact_ids) throws Exception;
+	void updateContactDetails();
 	}
