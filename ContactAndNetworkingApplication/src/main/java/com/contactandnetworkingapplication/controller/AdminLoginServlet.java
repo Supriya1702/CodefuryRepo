687fa9752afd0485da8236b82f521693c101b506
Simran Khetrapal <simrankhetrapal1999@gmail.com>
2021-09-26 19:40:03 +0530

    Integrating cotrollers Final

--- ContactAndNetworkingApplication/src/main/java/com/contactandnetworkingapplication/controller/AdminLoginServlet.java
+++ ContactAndNetworkingApplication/src/main/java/com/contactandnetworkingapplication/controller/AdminLoginServlet.java
@@ -23,24 +23,15 @@ import com.contactandnetworkingapplication.utility.DaoFactory;
 public class AdminLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AdminLoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request,response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		String username;
